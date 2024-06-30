d66b2178cb2f0038cab0b119c5dc35009bcca371
duong <hvdvietnamquetoi@gmail.com>
2024-06-30 13:23:00 +0700

    add client side

---
+++ frontend/web-app/next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: true
+    },
+    logging: {
+        fetches: {
+            fullUrl: true,
+        },
+    },
+    images: {
+        domains: [
+            'cdn.pixabay.com'
+        ]
+    }
+};
+
+export default nextConfig;
+
