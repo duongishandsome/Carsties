57a19925df98e17a3c0f033efd4f133cc2365a0a
duong <hvdvietnamquetoi@gmail.com>
2024-07-02 00:00:45 +0700

    add NextAuth

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { AiOutlineCar } from 'react-icons/ai'
 import Search from './Search'
 import Logo from './Logo'
+import LoginButton from './LoginButton'
+import { getCurrentUser } from '../actions/authActions'
+import UserAction from './UserAction'
 
-export default function Navbar() {
+export default async function Navbar() {
+    const user = await getCurrentUser();
+
     return (
         <header className='
-            sticky top-0 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
+            sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
         '>
             <Logo />
             <Search />
-            <div>Login</div>
+            {user ? (
+                <UserAction user={user} />
+            ) : (
+                <LoginButton />
+            )}
+
         </header>
     )
 }
--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -11,7 +11,7 @@ public class SearchController : ControllerBase
     public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
         var query = DB.PagedSearch<Item, Item>();
-        
+
         if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
@@ -19,25 +19,25 @@ public class SearchController : ControllerBase
 
         query = searchParams.OrderBy switch
         {
-            "make" => query.Sort(x => x.Ascending(a => a.Make)),
-            "new"  => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            "make" => query.Sort(x => x.Ascending(a => a.Make)).Sort(x => x.Ascending(a => a.Model)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
             _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
         };
 
         query = searchParams.FilterBy switch
         {
             "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) 
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
                 && x.AuctionEnd > DateTime.UtcNow),
             _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow),
         };
 
-        if(!string.IsNullOrEmpty(searchParams.Seller))
+        if (!string.IsNullOrEmpty(searchParams.Seller))
         {
             query.Match(x => x.Seller == searchParams.Seller);
         }
 
-         if(!string.IsNullOrEmpty(searchParams.Winner))
+        if (!string.IsNullOrEmpty(searchParams.Winner))
         {
             query.Match(x => x.Seller == searchParams.Winner);
         }
